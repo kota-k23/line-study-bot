48fc40094bc790cebf6fb723513a0872f43b777c
kota-k23 <kota.k.nrt.ars_1123@docomo.ne.jp>
2025-07-07 05:09:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -68,5 +68,8 @@ def handle_sticker(event):
         TextSendMessage(text=reply)
     )
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
